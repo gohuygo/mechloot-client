59b6818a1edf4eb22604364d3a5bd0bc9d235e7f
Huy Nguyen <huy@gohuygo.com>
2016-10-18 18:56:37 -0700

    react-router

--- src/App.spec.js
+++ src/App.spec.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
+import thunk from 'redux-thunk';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <Provider store={createStore(rootReducer)}>
+    <Provider store={createStore(rootReducer, applyMiddleware(thunk))}>
       <App />
     </Provider>, div);
 });
--- src/components/SubscriptionBanner.js
+++ src/components/SubscriptionBanner.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { setSubscribed } from '../actions/subscriptions'
 
 const SubscriptionBanner = ({handleSubmit, subscribed}) => {
+  let input
   if(subscribed){
     return (
      //TODO: Refactor as generic BannerMessage component
@@ -15,42 +16,41 @@ const SubscriptionBanner = ({handleSubmit, subscribed}) => {
   }else{
     return (
       //TODO: Refactor to SubscribeForm component
+      // Look into how this works: ref={(node) => {input = node}}
       <div className='row'>
         <div className='cover-container col-lg-12'>
           <p className='cta-text text-center'><span>Subscribe to get updates on Giveaways and Sales!</span></p>
 
           <div className='subscription-form-container'>
-            <form onSubmit={(e) => handleSubmit(e)} className='form-inline subscription-form'>
-              <input type='text' placeholder='email' className='form-control'/>
+            <form onSubmit={(e) => handleSubmit(e, input.value)} className='form-inline subscription-form'>
+              <input type='text' ref={(node) => {input = node}} placeholder='email' className='form-control'/>
               <input type="submit" name="commit" className='btn btn-default'/>
             </form>
           </div>
+
         </div>
       </div>
     )
   }
 }
 
-
-
-
 SubscriptionBanner.propTypes = {
   handleSubmit: PropTypes.func.isRequired,
-  subscribed: PropTypes.bool.isRequired
+  subscribed: PropTypes.bool.isRequired,
 }
 
 const mapStateToProps = (state) => {
   return {
-    subscribed: state.subscribed
+    subscribed: state.subscribed,
+    email: state.email
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    handleSubmit: (e) => {
+    handleSubmit: (e, email) => {
       e.preventDefault()
-      dispatch(setSubscribed(true))
-      // TODO: persist to DB with API call
+      dispatch(setSubscribed(true, email))
     }
   };
 };
