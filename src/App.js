594563955535506509cf91a65d4fee510aee7d60
Huy Nguyen <huy@gohuygo.com>
2016-10-27 14:41:19 -0700

    debugger and fixes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar.js';
 import ProductList from './components/ProductList.js';
+import CssDebugger from './utilities/CssDebugger.js';
 import './App.css';
 
 const App = () => (
   <div>
     <Navbar brand='PIRATE RADIO'/>
     <ProductList />
-    <span className="visible-xs">SIZE XS</span><span className="visible-sm">SIZE SM</span><span className="visible-md">SIZE MD</span><span className="visible-lg">SIZE LG</span>
+    <CssDebugger />
   </div>
 )
 
