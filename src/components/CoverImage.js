32b3256732f916751272d5488b24b4a464ce3c27
Huy Nguyen <huy@gohuygo.com>
2016-12-23 16:51:02 -0800

    more stuff

--- src/components/CoverImage.js
+++ src/components/CoverImage.js
@@ -5,7 +5,8 @@ class CoverImage extends React.Component {
   render() {
     return(
       <div className='cover-container col-lg-12'>
-        <p className='cta-text center-text'>Homes for your next masterpiece.</p>
+        <p className='cta-text center-text'>Exclusive art showings with local emerging artists</p>
+        <p className='center-text'>Of course we'll serve wine.</p>
       </div>
     )
   }
